442f24e3d83399ac88d81fb9aa684bcad13256ad
Silioni Evi <SilioniE@unisystems.gr>
2018-08-17 12:35:40 +0300

    Looping in js

---
+++ Loop.js
@@ -0,0 +1,27 @@
+// For Loop with increment value
+
+// Print a table of any number 
+function anyNumberTbl(){
+    var number = 8;
+
+    for(i=1; i<=10;i++){
+        console.log(number*i);
+    };
+};
+
+// Print a table of any number but in reverse ordrer
+
+function anyReverseNumberTbl(){
+    var number = 8;
+
+    for(i=10; i>=1;i--){
+        console.log(number*i);
+    };
+};
+
+
+anyReverseNumberTbl();
+
+ 
+
+
